e5a35fce3724ffb1749ebbe7e1f8152559a78071
Alex <alex.gerstl@gmx.at>
2020-03-24 20:45:30 +0100

    work in progress

--- Backup.py
+++ Backup.py
@@ -4,8 +4,8 @@ import msvcrt
 from filecmp import dircmp
 from termcolor import colored
 
-target_dir = 'X:\\Serien'
-#target_dir = 'C:\\Test'
+#target_dir = 'X:\\Serien'
+target_dir = 'C:\\Test'
 source_dir = 'F:\\Data\\Serien'
 source_root = [f.path for f in os.scandir(source_dir) if f.is_dir()]
 os.system('color')
@@ -34,15 +34,25 @@ def create_backup_tree(_series, _season, target):
             print(colored("Could not create folder for season %s of series %s" % (_season, _series), "red"))
         else:
             print(colored("Folder for season %s of series %s created" % (_season, _series), "yellow"))
-    return os.path.join(os.path.join(target_dir, _series), _season)
+
+
+def possible_backups():
+    _list = []
+    for s in source_root:
+        _series, _season = get_series_season(s)
+        target_dir_series_season = os.path.join(os.path.join(target_dir, _series), _season)
+        if not os.path.exists(target_dir_series_season):
+            _list.append(s)
+        else:
+            if len(get_diff(s, target_dir_series_season)) > 0:
+                _list.append(s)
+
+    return _list
 
 
 def get_size(folder):
-    _size = 0
     _files = os.listdir(folder)
-    for f in _files:
-        _size += os.path.getsize(os.path.join(folder, f))
-    return _size
+    return len(_files)
 
 
 def is_bigger(source, target):
@@ -84,31 +94,31 @@ def action_move(source, target, content):
 
 
 if __name__ == "__main__":
-    for s in source_root:
-        _series, _season = get_series_season(s)
-        _s = "Detected {} - {}".format(_series, _season)
-        print(_s.center(40, "-"))
-        print(colored("Backup (B) or do Nothing (N)? ", "yellow", "on_blue"))
+    print(colored("Listing possible backups:", "white"))
+    _p = possible_backups()
+    for _l in _p:
+        _series, _season = get_series_season(_l)
+        _s = "Detected: {} - {}".format(_series, _season)
+        print("\t" + _s)
+
+    for _l in _p:
+        _series, _season = get_series_season(_l)
+        print(colored(_series + " - " + _season + ": do Backup (B) or do Nothing (N)? ", "yellow", "on_blue"))
         action = msvcrt.getch()
         if ord(action.lower()) == ord("b"):
             create_backup_tree(_series, _season, target_dir)
-            target_dir_series = os.path.join(target_dir, _series)
-            target_dir_series_season = os.path.join(target_dir_series, _season)
-            diff = get_diff(s, target_dir_series_season)
-            if len(diff) > 0:
-                diff = get_diff(s, target_dir_series_season)
-                print(colored("Diff in series: %s" % _series, "yellow"), colored("%d file(s) local but not on NAS" % len(diff), "magenta"))
-                print()
-                action = request_input()
-                if action == ord("c"):
-                    print(colored("Start copying files", "grey"))
-                    action_copy(s, target_dir_series_season, diff)
-                if action == ord("m"):
-                    print(colored("Start moving files", "grey"))
-                    action_move(s, target_dir_series_season, diff)
-                if action == ord("n"):
-                    continue
-            else:
-                print(colored("No diff between local and NAS", "green"))
-        if action == "n":
+            target_dir_series_season = os.path.join(os.path.join(target_dir, _series), _season)
+            diff = get_diff(_l, target_dir_series_season)
+            print(colored("Diff in series: %s" % _series, "yellow"),
+                  colored("%d file(s) local but not on NAS" % len(diff), "magenta"))
+            action = request_input()
+            if action == ord("c"):
+                print(colored("Start copying files", "grey"))
+                action_copy(_l, target_dir_series_season, diff)
+            if action == ord("m"):
+                print(colored("Start moving files", "grey"))
+                action_move(_l, target_dir_series_season, diff)
+            if action == ord("n"):
+                continue
+        if ord(action.lower()) == ord("n"):
             continue

4603d117ac31ccd29c1efcb38a44b4df464fc406
Alex <alex.gerstl@gmx.at>
2020-03-24 21:15:24 +0100

    added progress bar

--- Backup.py
+++ Backup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 import msvcrt
 from filecmp import dircmp
@@ -9,6 +10,40 @@ target_dir = 'C:\\Test'
 source_dir = 'F:\\Data\\Serien'
 source_root = [f.path for f in os.scandir(source_dir) if f.is_dir()]
 os.system('color')
+
+
+class ProgressBar(object):
+    def __init__(self, message, width=20, progressSymbol='+', emptySymbol='-'):
+        self.width = width
+
+        if self.width < 0:
+            self.width = 0
+
+        self.message = message
+        self.progressSymbol = progressSymbol
+        self.emptySymbol = emptySymbol
+
+    def update(self, progress):
+        totalBlocks = self.width
+        filledBlocks = int(round(progress / (100 / float(totalBlocks))))
+        emptyBlocks = totalBlocks - filledBlocks
+
+        progressBar = self.progressSymbol * filledBlocks + \
+                      self.emptySymbol * emptyBlocks
+
+        if not self.message:
+            self.message = u''
+
+        progressMessage = u'\r{0} {1}  {2}%'.format(self.message,
+                                                    progressBar,
+                                                    progress)
+
+        sys.stdout.write(progressMessage)
+        sys.stdout.flush()
+
+    def calculateAndUpdate(self, done, total):
+        progress = int(round((done / float(total)) * 100))
+        self.update(progress)
 
 
 def get_series_season(source):
@@ -76,19 +111,29 @@ def request_input():
 
 
 def action_copy(source, target, content):
+    p = ProgressBar('Copying files...')
+    numCopied = 0
     for f in content:
         try:
             print(colored("Copying file %s" % f, "blue"))
             shutil.copy(os.path.join(source, f), os.path.join(target, f))
+            numCopied += 1
+            p.calculateAndUpdate(numCopied, len(content))
+            print()
         except:
             print(colored("Error while copying file: %s" % f, "red"))
 
 
 def action_move(source, target, content):
+    p = ProgressBar('Moving files...')
+    numCopied = 0
     for f in content:
         try:
             print(colored("Moving file %s" % f, "blue"))
             shutil.move(os.path.join(source, f), os.path.join(target, f))
+            numCopied += 1
+            p.calculateAndUpdate(numCopied, len(content))
+            print()
         except:
             print(colored("Error while moving file: %s" % f, "red"))
 
@@ -99,13 +144,14 @@ if __name__ == "__main__":
     for _l in _p:
         _series, _season = get_series_season(_l)
         _s = "Detected: {} - {}".format(_series, _season)
-        print("\t" + _s)
+        print(colored("\t" + _s, "yellow"))
+    print()
 
     for _l in _p:
         _series, _season = get_series_season(_l)
         print(colored(_series + " - " + _season + ": do Backup (B) or do Nothing (N)? ", "yellow", "on_blue"))
-        action = msvcrt.getch()
-        if ord(action.lower()) == ord("b"):
+        _action = msvcrt.getch()
+        if ord(_action.lower()) == ord("b"):
             create_backup_tree(_series, _season, target_dir)
             target_dir_series_season = os.path.join(os.path.join(target_dir, _series), _season)
             diff = get_diff(_l, target_dir_series_season)
@@ -120,5 +166,7 @@ if __name__ == "__main__":
                 action_move(_l, target_dir_series_season, diff)
             if action == ord("n"):
                 continue
-        if ord(action.lower()) == ord("n"):
+            print()
+        if ord(_action.lower()) == ord("n"):
             continue
+    print(colored("Done!!", "yellow"))
